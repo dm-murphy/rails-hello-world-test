dd120e7e224e812e187c739f89a4abe73da949db
Daniel Murphy <danielmichaelmurphy@gmail.com>
2021-12-23 13:34:13 -0500

    Add test for user#full_name

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#full_name' do
+    it 'concatenates first and last name' do
+      user = User.new(first_name: 'Daniel', last_name: 'Murphy')
+      expect(user.full_name).to eq('Daniel Murphy')
+    end
+  end
 end
